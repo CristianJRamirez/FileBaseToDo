26d9512c4936ee5be0ee8874c263b251e6125341
CristianJRamirez <45858000w@iespoblenou.org>
2017-02-14 18:16:12 +0100

    añadidas funciones de camara y video y hacer foto y video y guardarlos

--- app/src/main/java/a45858000w/filebasetodo/MainActivityFragment.java
+++ app/src/main/java/a45858000w/filebasetodo/MainActivityFragment.java
@@ -1,6 +1,12 @@
 package a45858000w.filebasetodo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +18,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
@@ -19,8 +26,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+
+import static android.R.attr.data;
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 
 /**
@@ -32,6 +52,8 @@ public class MainActivityFragment extends Fragment {
     private ListView list;
     private EditText txtInsert;
     private Button btAdd;
+    private Button btVideo;
+    private Button btCamara;
     private ArrayList<String> listaDatos;
 
     //ArrayAdapter<String> adapter;
@@ -39,7 +61,10 @@ public class MainActivityFragment extends Fragment {
 
     private int RC_Sign_in =123;
     private FirebaseAuth auth;
-
+    private String pathFotoTemporal;
+    private String pathVideoTemporal;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
 
     public MainActivityFragment() {
     }
@@ -57,6 +82,10 @@ public class MainActivityFragment extends Fragment {
         txtInsert= (EditText) view.findViewById(R.id.txtInsert);
         btAdd = (Button) view.findViewById(R.id.btAdd);
 
+        btCamara = (Button) view.findViewById(R.id.btCamara);
+
+        btVideo = (Button) view.findViewById(R.id.btVideo);
+
         setupAuth();
 
         listaDatos= new ArrayList<String>();
@@ -71,10 +100,6 @@ public class MainActivityFragment extends Fragment {
         };
 
         list.setAdapter(adapter);
-
-
-
-
 
 
 
@@ -99,6 +124,23 @@ public class MainActivityFragment extends Fragment {
                 }
             }
         });
+
+
+        btCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+        btVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeVideo();
+            }
+        });
+
 
 
 
@@ -132,8 +174,148 @@ public class MainActivityFragment extends Fragment {
 
 
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        pathFotoTemporal = "file:" + image.getAbsolutePath();
+        return image;
+    }
 
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    private void takeVideo ()
+    {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takeVideoIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File videoFile = null;
+            try {
+                videoFile = createVideoFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (videoFile != null) {
+                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(videoFile));// set the image file name
+                takeVideoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1); // set the video image quality to high
+
+                startActivityForResult(takeVideoIntent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+            }
+        }
+    }
+
+
+    private File createVideoFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        String videoFileName = "Video_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                videoFileName,  /* prefix */
+                ".mp4",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        pathVideoTemporal = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+
+
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)  {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+            try {
+
+                if (requestCode == REQUEST_TAKE_PHOTO) {
+                    if (resultCode == RESULT_OK) {
+
+                        //Uri seleccio = intent.getData();
+
+                        Uri.Builder b = Uri.parse(pathFotoTemporal).buildUpon();
+
+                        String[] columna = {MediaStore.Images.Media.DATA};
+
+                        Cursor cursor = getContext().getContentResolver().query(b.build(), columna, null, null, null);
+                        cursor.moveToFirst();
+
+                        int indexColumna = cursor.getColumnIndex(columna[0]);
+                        String rutaFitxer = cursor.getString(indexColumna);
+                        cursor.close();
+                    }
+                }
+
+
+                if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+                    if (resultCode == RESULT_OK) {
+                        // Video captured and saved to fileUri specified in the Intent
+                        //Toast.makeText(this, "Video saved to:\n" +data.getData(), Toast.LENGTH_LONG).show();
+
+                        Uri.Builder b = Uri.parse(pathVideoTemporal).buildUpon();
+
+                        String[] columna = {MediaStore.Images.Media.DATA};
+
+                        Cursor cursor = getContext().getContentResolver().query(b.build(), columna, null, null, null);
+                        cursor.moveToFirst();
+
+                        int indexColumna = cursor.getColumnIndex(columna[0]);
+                        String rutaFitxer = cursor.getString(indexColumna);
+                        cursor.close();
+
+                    } else if (resultCode == RESULT_CANCELED) {
+                        // User cancelled the video capture
+                    } else {
+                        // Video capture failed, advise user
+                    }
+                }
+
+
+            }catch (Exception e) {
+                e.printStackTrace();
+
+            }
+
+    }
 
 
 
